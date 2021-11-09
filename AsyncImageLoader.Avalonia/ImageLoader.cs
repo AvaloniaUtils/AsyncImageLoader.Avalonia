ca3c9e5b4ddc5ea85338e3f6cb0f1eba49a272e1
SKProCH <skproch@yandex.ru>
2021-11-09 17:51:12 +0300

    Change property name to Source, fix accessing ui in invalid thread

--- AsyncImageLoader.Avalonia/ImageLoader.cs
+++ AsyncImageLoader.Avalonia/ImageLoader.cs
@@ -8,26 +8,27 @@ namespace AsyncImageLoader.Avalonia {
     public static class ImageLoader {
         public static IAsyncImageLoader AsyncImageLoader { get; set; } = new RamCachedWebImageLoader();
         static ImageLoader() {
-            SourceUrlProperty.Changed.Subscribe(args => {
+            SourceProperty.Changed.Subscribe(args => {
                 _ = AsyncImageLoader.ProvideImageAsync(args.NewValue.Value)
                     .ContinueWith(task => {
                         var bitmap = task.GetAwaiter().GetResult();
-                        if (GetSourceUrl((args.Sender as Image)!) != args.NewValue.Value) return;
+                        var image = (args.Sender as Image)!;
                         Dispatcher.UIThread.Post(() => {
-                            (args.Sender as Image)!.Source = bitmap;
+                            if (GetSource(image) != args.NewValue.Value) return;
+                            image.Source = bitmap;
                         });
                     }, TaskContinuationOptions.OnlyOnRanToCompletion);
             });
         }
 
-        public static readonly AttachedProperty<string> SourceUrlProperty = AvaloniaProperty.RegisterAttached<Image, string>("SourceUrl", typeof(ImageLoader));
+        public static readonly AttachedProperty<string> SourceProperty = AvaloniaProperty.RegisterAttached<Image, string>("Source", typeof(ImageLoader));
 
-        public static string GetSourceUrl(Image element) {
-            return element.GetValue(SourceUrlProperty);
+        public static string GetSource(Image element) {
+            return element.GetValue(SourceProperty);
         }
 
-        public static void SetSourceUrl(Image element, string value) {
-            element.SetValue(SourceUrlProperty, value);
+        public static void SetSource(Image element, string value) {
+            element.SetValue(SourceProperty, value);
         }
     }
 }
